3110540ce41928d90531f3227f3b14cb14d8f072
wanguimbutu <wanguimbutu@gmail.com>
2025-01-15 15:01:02 +0300

    add cost calculation to booking inquiry

--- tours_and_safaris/tours_and_safaris/doctype/booking_inquiry/booking_inquiry.js
+++ tours_and_safaris/tours_and_safaris/doctype/booking_inquiry/booking_inquiry.js
@@ -6,6 +6,9 @@ frappe.ui.form.on('Booking Inquiry', {
         // Hide both fields initially
         frm.set_df_property('no_of_tents_needed', 'hidden', 1);
         frm.set_df_property('room_booking', 'hidden', 1);
+
+        // Calculate total cost on form refresh
+        calculate_total_cost(frm);
     },
     accommodation_type: function(frm) {
         // Hide both fields initially
@@ -18,5 +21,48 @@ frappe.ui.form.on('Booking Inquiry', {
         } else if (frm.doc.accommodation_type === 'Tents') {
             frm.set_df_property('no_of_tents_needed', 'hidden', 0);
         }
+    },
+    // Recalculate total cost when activities table changes
+    activities_add: function(frm) {
+        calculate_total_cost(frm);
+    },
+    activities_remove: function(frm) {
+        calculate_total_cost(frm);
+    },
+    // Recalculate total cost when room booking table changes
+    room_booking_add: function(frm) {
+        calculate_total_cost(frm);
+    },
+    room_booking_remove: function(frm) {
+        calculate_total_cost(frm);
+    },
+    // Recalculate when cost or price values are changed
+    'activities.*.cost': function(frm) {
+        calculate_total_cost(frm);
+    },
+    'room_booking.*.price': function(frm) {
+        calculate_total_cost(frm);
     }
 });
+
+// Function to calculate total cost
+function calculate_total_cost(frm) {
+    let total_cost = 0;
+
+    // Sum up the cost from activities table
+    if (frm.doc.activities) {
+        frm.doc.activities.forEach(activity => {
+            total_cost += flt(activity.cost);
+        });
+    }
+
+    // Sum up the price from room booking table
+    if (frm.doc.room_booking) {
+        frm.doc.room_booking.forEach(room => {
+            total_cost += flt(room.price);
+        });
+    }
+
+    // Update the proposed total cost field
+    frm.set_value('proposed_total_cost', total_cost);
+}
