70dd0fa31f7c2436ad6af217329335e6a09a9d9c
wanguimbutu <wanguimbutu@gmail.com>
2025-01-15 14:36:17 +0300

    show fields depending on selected option in booking inquiry

--- tours_and_safaris/tours_and_safaris/doctype/booking_inquiry/booking_inquiry.js
+++ tours_and_safaris/tours_and_safaris/doctype/booking_inquiry/booking_inquiry.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, wanguimbutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Booking Inquiry", {
-// 	refresh(frm) {
+frappe.ui.form.on('Booking Inquiry', {
+    refresh: function(frm) {
+        // Hide both fields initially
+        frm.set_df_property('no_of_tents_needed', 'hidden', 1);
+        frm.set_df_property('room_booking', 'hidden', 1);
+    },
+    accommodation_type: function(frm) {
+        // Hide both fields initially
+        frm.set_df_property('no_of_tents_needed', 'hidden', 1);
+        frm.set_df_property('room_booking', 'hidden', 1);
 
-// 	},
-// });
+        // Show fields based on the selected accommodation type
+        if (frm.doc.accommodation_type === 'Rooms') {
+            frm.set_df_property('room_booking', 'hidden', 0);
+        } else if (frm.doc.accommodation_type === 'Tents') {
+            frm.set_df_property('no_of_tents_needed', 'hidden', 0);
+        }
+    }
+});
