4d0519c79c3a80d6fb527a16a1e892055a40919f
wanguimbutu <wanguimbutu@gmail.com>
2025-02-18 16:13:34 +0300

    Match fields to papertrail.

---
+++ tours_and_safaris/tours_and_safaris/doctype/equipment_type_folder/equipment_type_folder.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, wanguimbutu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Equipment Type Folder", {
+// 	refresh(frm) {
+
+// 	},
+// });
