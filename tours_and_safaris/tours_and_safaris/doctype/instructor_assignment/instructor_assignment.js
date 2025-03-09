6c568e30387c1b2dc20fda33584a784992f2dce0
wanguimbutu <wanguimbutu@gmail.com>
2025-03-09 15:09:50 +0300

    equipment allocation and instructor assignment

--- tours_and_safaris/tours_and_safaris/doctype/instructor_assignment/instructor_assignment.js
+++ tours_and_safaris/tours_and_safaris/doctype/instructor_assignment/instructor_assignment.js
@@ -1,29 +1,3 @@
 // Copyright (c) 2025, wanguimbutu and contributors
 // For license information, please see license.txt
 
-
-frappe.ui.form.on("Instructor Assignment", {
-    refresh: function(frm) {
-        frm.add_custom_button("Fetch All Instructors", function() {
-            frappe.call({
-                method: "tours_and_safaris.tours_and_safaris.doctype.instructor_assignment.instructor_assignment.get_all_instructors",
-                args: {
-                    activity: frm.doc.activity,
-                    session_type: frm.doc.session_type
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.clear_table("instructor_suggestions");
-                        r.message.forEach((row) => {
-                            let child = frm.add_child("instructor_suggestions");
-                            child.instructor = row.instructor;
-                            child.qualification = row.qualification;
-                        });
-                        frm.refresh_field("instructor_suggestions");
-                        frappe.msgprint("Instructor suggestions updated. You can now manually add or remove instructors.");
-                    }
-                }
-            });
-        });
-    }
-});
--- tours_and_safaris/tours_and_safaris/doctype/instructor_assignment/instructor_assignment.py
+++ tours_and_safaris/tours_and_safaris/doctype/instructor_assignment/instructor_assignment.py
@@ -1,32 +1,10 @@
 # Copyright (c) 2025, wanguimbutu and contributors
 # For license information, please see license.txt
 
-import frappe
+#import frappe
 from frappe.model.document import Document
 
 
 class InstructorAssignment(Document):
 	pass
 
-@frappe.whitelist()
-def get_all_instructors(activity=None, session_type=None):
-    frappe.logger().info(f"Received parameters - Activity: {activity}, Session Type: {session_type}")
-
-    # Explicitly check for None
-    if not activity or not session_type:
-        frappe.throw(f"Both activity and session_type parameters are required. Received: Activity={activity}, Session Type={session_type}")
-
-    instructors = frappe.db.sql("""
-        SELECT 
-            ia.parent AS instructor, 
-            ia.qualification, 
-            ir.rate  
-        FROM `tabInstructor Activity Level` ia
-        LEFT JOIN `tabInstructor Rate` ir 
-            ON ia.qualification = ir.qualification 
-            AND ia.activity_name = ir.activity_name 
-            AND ir.session_type = %s
-        WHERE ia.activity_name = %s
-    """, (session_type, activity), as_dict=True)
-
-    return instructors
