d24f4b515b7467eaf6f1a11b5afd796819555c07
wanguimbutu <wanguimbutu@gmail.com>
2025-03-28 11:34:15 +0300

    task completion on submit

--- tours_and_safaris/tours_and_safaris/doctype/equipment_log/equipment_log.js
+++ tours_and_safaris/tours_and_safaris/doctype/equipment_log/equipment_log.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2025, wanguimbutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Equipment Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Equipment Log", {
+    after_save: function(frm) {
+        if (frm.doc.task_name) {
+            frappe.call({
+                method: "frappe.client.set_value",
+                args: {
+                    doctype: "Task",
+                    name: frm.doc.task_name,
+                    fieldname: "status",
+                    value: "Completed"
+                },
+                callback: function(response) {
+                    if (!response.exc) {
+                        frappe.show_alert({message: "Task marked as Working", indicator: "green"});
+                    }
+                }
+            });
+        }
+    }
+});
