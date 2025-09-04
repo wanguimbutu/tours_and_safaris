0880e66b3063ffc78aaa07a73b710f8fdb2e754f
wanguimbutu <wanguimbutu@gmail.com>
2025-09-04 15:36:56 +0300

    calendar namefor room allocation

--- tours_and_safaris/tours_and_safaris/doctype/room_allocation/room_allocation.py
+++ tours_and_safaris/tours_and_safaris/doctype/room_allocation/room_allocation.py
@@ -1,16 +1,28 @@
-# Copyright (c) 2025, wanguimbutu and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 from frappe import _
 
 class RoomAllocation(Document):
-	pass
-
-class RoomAllocation(Document):
     def on_submit(self):
         for booked_room in self.booked_rooms:
+            # Check if this room is already booked in the given date range
+            existing = frappe.get_all(
+                "Availability",
+                filters={
+                    "room_name": booked_room.room_name,
+                    "check_in_date": ["<=", self.departure_date],
+                    "check_out_date": [">=", self.arrival_date]
+                },
+                limit=1
+            )
+
+            if existing:
+                frappe.throw(
+                    _("Room {0} is already allocated between {1} and {2}. Please choose another room.")
+                    .format(booked_room.room_name, self.arrival_date, self.departure_date)
+                )
+
+            # If not booked, create the availability record
             availability = frappe.new_doc("Availability")
             availability.room_name = booked_room.room_name  
             availability.reservation = self.reservation     
@@ -18,32 +30,33 @@ class RoomAllocation(Document):
             availability.check_out_date = self.departure_date
             availability.customer = self.customer
             availability.calendar_info = self.calendar_info
-            availability.insert(ignore_permissions=True) 
-            availability.submit() 
-            
-pass
 
+            availability.group_room = f"{self.customer}-{booked_room.room_name}"
+            
+            availability.insert(ignore_permissions=True) 
+            availability.submit()
 
 @frappe.whitelist()
 def get_available_rooms(room_type, check_in_date, check_out_date):
+    # get all rooms of this type
     available_rooms = frappe.get_all(
         "Rooms",  
-        filters={
-            "room_type": room_type
-        },
+        filters={"room_type": room_type},
         pluck="name"
     )
 
+    # get rooms already booked in the given date range
     booked_rooms = frappe.get_all(
         "Availability", 
         filters={
-            "room_type": room_type,
+            "room_name": ["in", available_rooms],   
             "check_in_date": ["<=", check_out_date], 
-            "check_out_date": [">=", check_in_date]   
+            "check_out_date": [">=", check_in_date]
         },
         pluck="room_name"
     )
 
+    # final set = all rooms - booked rooms
     final_rooms = list(set(available_rooms) - set(booked_rooms))
 
     return final_rooms
